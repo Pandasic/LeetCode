fde9c80ac4edf46bcb3b5eca7f805bf6d7134724
TB521 <940706604@qq.com>
2019-06-03 13:52:57 +0800

    add README.md

--- 51 N皇后问题.py
+++ 51 N皇后问题.py
@@ -1,20 +1,22 @@
-class Solution:
-    def __init__(self):
+class NQueens:
+    def __init__(self,n = 8):
         self.chess = []
         self.width = 0
         self.count = 0
+        self.results = []
+        self.solveNQueens(n)
+
     def solveNQueens(self, n):
         self.width = n
         self.chess =[ [0 for i in range(n)] for i in range(n)]
         self.queens()
-        print(self.count)
+        return self.results
     
     def queens(self,y = 0,queens = []):
         for x in range(self.width):
             if self.check(x,y,queens):#位置不冲突
-                print("%d,%d 成功"%(x,y))
                 if y == self.width-1:
-                    print(queens)
+                    self.results += [queens+[(x,y)]]
                     self.count += 1
                 self.queens(y + 1,queens[:] + [(x,y)])
     
@@ -24,5 +26,5 @@ class Solution:
                 return False 
         return True
             
-s = Solution()
-s.solveNQueens(8)
+s = NQueens(8)
+print(s.results)
---
+++ README.md
@@ -0,0 +1,30 @@
+<b>LeetCode 刷题</b>
+----------------------------
+
+序号|题目|语言
+:--:|:--:|---:
+35| |python
+36| |python
+37| |python
+39| |python
+40|组合总和II|python
+41|缺失的第一个正数|python
+42|接雨水|python
+43|字符串相乘|python
+44|通配符匹配|python
+45|跳跃游戏2|python
+46|全排列|python
+47|全排列2|python
+48|旋转矩阵|python
+49|字母异位分组|python
+51|N皇后问题|python
+53|最大子序列和|python
+1029|两地调度|python
+5024|除数博弈|python
+5025|最长等差数列|python
+5030|节点与其祖先之间的最大差值|python
+5055|困于环中的机器人|python
+5056|不邻接种花|python
+5063|最后一块石头的重量|python
+5064|删除字符串中所有相邻的重复项|python
+5065|最长字符串链子|python
